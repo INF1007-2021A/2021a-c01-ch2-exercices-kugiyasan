9f72de7e200bcdf66c6bd8951b2b8fbca1304b50
kugiyasan <kugiyasan@users.noreply.github.com>
2021-09-07 17:04:38 -0400

    Wrote the majuscule function in exercice.py

--- exercice.py
+++ exercice.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-def majuscule(mot):
-    # TODO completer la fonction ici
-    return mot
 
 
-if __name__ == '__main__':
+def majuscule(mot: str) -> str:
+    """
+    Returns the uppercase version of the string
+    This function does the same thing as str.upper
+    """
+    result = ""
+    for c in mot:
+        if c.islower():
+            # Uppercase code + 32 = lowercase letter
+            c = chr(ord(c) - 32)
+        result += c
+    return result
+
+
+if __name__ == "__main__":
     mots = [
-        'riz',
-        'cours',
-        'voiture',
-        'oiseau',
-        'bonjour',
-        'églantier',
-        'arbre'
+        "riz",
+        "cours",
+        "voiture",
+        "oiseau",
+        "bonjour",
+        "églantier",
+        "arbre",
+        "_-_!@#$%?&*()ÉE_-_",
     ]
     for i in range(len(mots)):
         mots[i] = majuscule(mots[i])
